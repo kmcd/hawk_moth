6be8fc204146e2097becf4a1f4e9aaee08e4d7b6
Keith Mc Donnell <keith@dancingtext.com>
2012-01-24 15:00:47 +0000

    Using spread ma instead of Z-score

--- lib/pair.rb
+++ lib/pair.rb
@@ -5,20 +5,19 @@ class Pair
     @ticker_1, @ticker_2 = ticker_1,ticker_2
   end
   
-  def spread_zscore(time_stamp)
+  def spread_at(time_stamp)
     return 0.0 if market_opening? time_stamp
     @quotes = intraday_quotes_upto(time_stamp)
-    dist  = spreads.to_scale
     
-    (spreads.last - dist.mean) / dist.standard_deviation_sample
+    spreads.last - spreads.to_scale.mean
   end
   
   def quotes_and_spread_at(time_stamp)
-    zscore = spread_zscore time_stamp
+    spread = spread_at time_stamp
     quotes = market_opening?(time_stamp) ? intraday_quotes_upto(time_stamp) : @quotes
     
     quotes[-2..-1].map {|quote| [quote.ticker.downcase.to_sym, quote.close] }.
-      push zscore.round(4)
+      push spread.round(4)
   end
   
   private
--- test/pair_test.rb
+++ test/pair_test.rb
@@ -5,67 +5,67 @@ class PairTest < Test::Unit::TestCase
     @pair = Pair.new 'SPY', 'IVV'
   end
   
-  test "should have cumulative spread of zero on market open" do
-    quote "2011-11-14 09:30:00", "SPY", 133.97
-    quote "2011-11-14 09:30:00", "IVV", 134.4036
+  test "should have spread of zero on market open" do
+    quote "2011-07-08 09:30:00", "SPY", 133.97
+    quote "2011-07-08 09:30:00", "IVV", 134.4036
     
-    assert_equal 0.0, @pair.spread_zscore("2011-11-14 09:30:00")
+    assert_equal 0.0, @pair.spread_at("2011-07-08 09:30:00")
   end
   
-  test "should calculate Z-score of cumulative spread price CHANGES" do
-    quote "2011-11-14 09:30:00", "SPY", 133.97
-    quote "2011-11-14 09:30:00", "IVV", 134.4036
-    quote "2011-11-14 09:31:00", "SPY", 134.01	
-    quote "2011-11-14 09:31:00", "IVV", 134.46
+  test "should calculate difference between current spread and moving average" do
+    quote "2011-07-08 09:30:00", "SPY", 133.97
+    quote "2011-07-08 09:30:00", "IVV", 134.4036
+    quote "2011-07-08 09:31:00", "SPY", 134.01	
+    quote "2011-07-08 09:31:00", "IVV", 134.46
     
-    assert_in_delta -0.7071, @pair.spread_zscore("2011-11-14 09:31:00"), 0.0001
+    assert_in_delta -0.0082, @pair.spread_at("2011-07-08 09:31:00"), 0.0001
   end
   
-  test "should calculate Z-score of cumulative spread running total" do
-    quote "2011-11-14 09:30:00", "SPY", 133.97
-    quote "2011-11-14 09:30:00", "IVV", 134.4036
-    quote "2011-11-14 09:31:00", "SPY", 134.01	
-    quote "2011-11-14 09:31:00", "IVV", 134.46
-    quote "2011-11-14 09:32:00", "SPY", 133.98
-    quote "2011-11-14 09:32:00", "IVV", 134.451
+  test "should calculate moving average from market open" do
+    quote "2011-07-08 09:30:00", "SPY", 133.97
+    quote "2011-07-08 09:30:00", "IVV", 134.4036
+    quote "2011-07-08 09:31:00", "SPY", 134.01	
+    quote "2011-07-08 09:31:00", "IVV", 134.46
+    quote "2011-07-08 09:32:00", "SPY", 133.98
+    quote "2011-07-08 09:32:00", "IVV", 134.451
     
-    assert_in_delta -1.0384, @pair.spread_zscore("2011-11-14 09:32:00"), 0.0001
+    assert_in_delta -0.0195, @pair.spread_at("2011-07-08 09:32:00"), 0.0001
   end
   
   test "should only use current day for spread calculations" do
-    quote "2011-11-14 16:00:00", "SPY", 133.97
-    quote "2011-11-14 16:00:00", "IVV", 134.4036
+    quote "2011-07-08 16:00:00", "SPY", 133.97
+    quote "2011-07-08 16:00:00", "IVV", 134.4036
     quote "2011-11-15 09:30:00", "SPY", 134.01	
     quote "2011-11-15 09:30:00", "IVV", 134.46
     
-    assert_equal 0.0, @pair.spread_zscore("2011-11-15 09:30:00")
+    assert_equal 0.0, @pair.spread_at("2011-11-15 09:30:00")
   end
   
   test "should ignore missing bars" do
-    quote "2011-11-14 09:30:00", "SPY", 133.97
-    quote "2011-11-14 09:30:00", "IVV", 134.4036
-    quote "2011-11-14 09:31:00", "SPY", 134.01
-    quote "2011-11-14 09:32:00", "SPY", 134.01	
-    quote "2011-11-14 09:32:00", "IVV", 134.46
+    quote "2011-07-08 09:30:00", "SPY", 133.97
+    quote "2011-07-08 09:30:00", "IVV", 134.4036
+    quote "2011-07-08 09:31:00", "SPY", 134.01
+    quote "2011-07-08 09:32:00", "SPY", 134.01	
+    quote "2011-07-08 09:32:00", "IVV", 134.46
     
-    assert_in_delta -0.7071, @pair.spread_zscore("2011-11-14 09:32:00"), 0.0001
+    assert_in_delta -0.0082, @pair.spread_at("2011-07-08 09:32:00"), 0.0001
   end
   
-  test "should detect a high/low spread Z-score" do
+  test "should detect a high/low spread" do
     load_fixtures
     
-    assert_in_delta -2.7358, @pair.spread_zscore("2011-07-08 10:59:00"), 0.0001
-    assert_in_delta -2.6945, @pair.spread_zscore("2011-07-08 11:09:00"), 0.0001
-    assert_in_delta 2.9627, @pair.spread_zscore("2011-07-08 11:40:00"), 0.0001
+    assert_in_delta -0.0319, @pair.spread_at("2011-07-08 10:59:00"), 0.0001
+    assert_in_delta -0.0319, @pair.spread_at("2011-07-08 11:09:00"), 0.0001
+    assert_in_delta 0.0359, @pair.spread_at("2011-07-08 11:40:00"), 0.0001
   end
   
   test "should have quotes and spread for a timestamp" do
-    quote "2011-11-14 09:30:00", "SPY", 133.97
-    quote "2011-11-14 09:30:00", "IVV", 134.4036
-    assert_equal [[:spy, 133.97], [:ivv, 134.4036],  0.0], @pair.quotes_and_spread_at("2011-11-14 09:30:00")
+    quote "2011-07-08 09:30:00", "SPY", 133.97
+    quote "2011-07-08 09:30:00", "IVV", 134.4036
+    assert_equal [[:spy, 133.97], [:ivv, 134.4036],  0.0], @pair.quotes_and_spread_at("2011-07-08 09:30:00")
     
-    quote "2011-11-14 09:31:00", "SPY", 134.01	
-    quote "2011-11-14 09:31:00", "IVV", 134.46
-    assert_equal [[:spy, 134.01], [:ivv, 134.46],  -0.7071], @pair.quotes_and_spread_at("2011-11-14 09:31:00")
+    quote "2011-07-08 09:31:00", "SPY", 134.01	
+    quote "2011-07-08 09:31:00", "IVV", 134.46
+    assert_equal [[:spy, 134.01], [:ivv, 134.46],  -0.0082], @pair.quotes_and_spread_at("2011-07-08 09:31:00")
   end
 end
